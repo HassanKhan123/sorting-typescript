379395ad97a098ee95041c387544d7d9f5b4b13f
HassanKhan123 <hk2602809@gmail.com>
2022-01-06 16:16:08 +0500

    added: linkedList sort

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 11]);
 const charactersCollection = new CharactersCollection('Hassan');
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(100);
+linkedList.add(600);
+linkedList.add(-500);
 
-const sorter = new Sorter(charactersCollection);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
