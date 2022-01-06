99ce84a24c0fe6894a83c96b868bc843c84f2f84
HassanKhan123 <hk2602809@gmail.com>
2022-01-06 15:55:54 +0500

    added: characters sort

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 11]);
+const charactersCollection = new CharactersCollection('Hassan');
 
-const sorter = new Sorter(numbersCollection);
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
